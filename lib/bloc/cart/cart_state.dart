57bb29d1ff026bfe35d634fd763e8a8898070281
Yesid.Rodelo <yesiddeveloper@gmail.com>
2021-03-06 23:23:50 -0500

    remove variable unused

--- lib/bloc/cart/cart_state.dart
+++ lib/bloc/cart/cart_state.dart
@@ -2,13 +2,11 @@ part of 'cart_bloc.dart';
 
 class CartState {
   final bool isPending;
-  final bool isCartProducts;
   final List<Products> cartProducts;
 
   CartState({ 
     bool pending = true,
     List<Products> productList,
   })  : isPending = pending,
-        cartProducts = (productList == null) ? [] : productList,
-        isCartProducts = (productList != null) ? true : false;
+        cartProducts = (productList == null) ? [] : productList;
 }
